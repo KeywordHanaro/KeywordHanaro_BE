21017d02ba4e63aec5d757e80e6c0a3747110d0a
ninw0205 <ninw0205@gmail.com>
2024-12-02 19:37:40 +0900

    Initial commit

---
+++ src/main/java/com/hana4/keywordhanaro/KeywordhanaroApplication.java
@@ -0,0 +1,13 @@
+package com.hana4.keywordhanaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeywordhanaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeywordhanaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hana4/keywordhanaro/KeywordhanaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana4.keywordhanaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeywordhanaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
