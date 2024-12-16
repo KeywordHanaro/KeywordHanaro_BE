8ff4a1ad2bb36261835522c43599dc7b5aca486f
mseoa <0527sophie@naver.com>
2024-12-16 19:05:25 +0900

    transaction entity column명 수정

--- src/main/java/com/hana4/keywordhanaro/model/entity/transaction/Transaction.java
+++ src/main/java/com/hana4/keywordhanaro/model/entity/transaction/Transaction.java
@@ -1,11 +1,9 @@
 package com.hana4.keywordhanaro.model.entity.transaction;
 
 import java.math.BigDecimal;
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 import org.hibernate.annotations.ColumnDefault;
-import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.CreationTimestamp;
 
 import com.hana4.keywordhanaro.model.entity.account.Account;
@@ -37,12 +35,12 @@ public class Transaction {
 	private Long id;
 
 	@ManyToOne
-	@JoinColumn(name = "from_account", nullable = false, foreignKey = @ForeignKey(name = "fk_Transaction_from_acount_Account"))
-	private Account fromAccount;
+	@JoinColumn(name = "account", nullable = false, foreignKey = @ForeignKey(name = "fk_Transaction_acount_Account"))
+	private Account account;
 
 	@ManyToOne
-	@JoinColumn(name = "to_account", nullable = false, foreignKey = @ForeignKey(name = "fk_Transaction_to_acount_Account"))
-	private Account toAccount;
+	@JoinColumn(name = "sub_account", foreignKey = @ForeignKey(name = "fk_Transaction_sub_acount_Account"))
+	private Account subAccount;
 
 	@Column(nullable = false, precision = 8, scale = 2)
 	private BigDecimal amount;
@@ -64,5 +62,4 @@ public class Transaction {
 	@Column(nullable = false, updatable = false, columnDefinition = "timestamp")
 	@ColumnDefault("CURRENT_TIMESTAMP")
 	private LocalDateTime createAt;
-
 }
