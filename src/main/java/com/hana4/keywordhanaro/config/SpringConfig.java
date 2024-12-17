71df6d039c506eea4ef802996df497e2cfe15bf7
ninw0205 <ninw0205@gmail.com>
2024-12-17 13:31:29 +0900

    [feat] : SwaggerUI 추가

---
+++ src/main/java/com/hana4/keywordhanaro/config/SpringConfig.java
@@ -0,0 +1,25 @@
+package com.hana4.keywordhanaro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SpringConfig {
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.components(new Components())
+			.info(info());
+	}
+
+	private Info info() {
+		return new Info()
+			.version("0.1.0")
+			.title("KeywordHanaro Api Spec.")
+			.description("description");
+	}
+}
