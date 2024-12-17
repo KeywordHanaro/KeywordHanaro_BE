d10179af8f4948ab728e23af63ad135baf3e8409
mseoa <0527sophie@naver.com>
2024-12-17 19:37:33 +0900

    swagger 추가

--- src/main/java/com/hana4/keywordhanaro/controller/InquiryController.java
+++ src/main/java/com/hana4/keywordhanaro/controller/InquiryController.java
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hana4.keywordhanaro.model.dto.TransactionDto;
 import com.hana4.keywordhanaro.service.InquiryServiceImpl;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+
 @RestController
 @RequestMapping("/inquiry")
 @ResponseStatus(HttpStatus.OK)
@@ -26,6 +29,18 @@ public class InquiryController {
 		this.inquiryService = inquiryService;
 	}
 
+	@Operation(
+		summary = "거래 내역 조회",
+		description = "조회기간, 거래구분, 정렬순서, 검색어로 필터링하여 조회합니다",
+		parameters = {
+			@Parameter(name = "accountId", description = "accountId", required = true, example = "9"),
+			@Parameter(name = "startDate", description = "검색 시작일 YYYY-mm-dd", required = true, example = "2024-01-01"),
+			@Parameter(name = "endDate", description = "검색 종료일 YYYY-mm-dd", required = true, example = "2024-12-31"),
+			@Parameter(name = "transactionType", description = "all-전체조회/deposit-입금만조회/withdraw-출금만조회", example = "all"),
+			@Parameter(name = "sortOrder", description = "latest-최신순/oldest 오래된순 정렬", example = "latest"),
+			@Parameter(name = "searchWord", description = "검색어", example = "밥값")
+		}
+	)
 	@GetMapping("/{accountId}")
 	public ResponseEntity<List<TransactionDto>> getAccountTransactions(
 		@PathVariable Long accountId,
