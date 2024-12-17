f7699163b408e0551a074409caa52c0732fa66c1
insun-k <kis8294@naver.com>
2024-12-17 13:35:09 +0900

    requestBody 수정

--- src/main/java/com/hana4/keywordhanaro/controller/TicketController.java
+++ src/main/java/com/hana4/keywordhanaro/controller/TicketController.java
@@ -2,23 +2,24 @@ package com.hana4.keywordhanaro.controller;
 
 import java.io.IOException;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hana4.keywordhanaro.model.dto.KeywordDTO;
 import com.hana4.keywordhanaro.model.dto.TicketDTO;
 import com.hana4.keywordhanaro.service.TicketService;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class TicketController {
 	private final TicketService ticketService;
 
-	public TicketController(TicketService ticketService) {
-		this.ticketService = ticketService;
-	}
-
 	@PostMapping("/ticket")
-	public TicketDTO createTicket(@RequestParam Long keywordId) throws IOException {
-		return ticketService.createTicket(keywordId);
+	public ResponseEntity<TicketDTO> createTicket(@RequestParam KeywordDTO keywordDTO) throws IOException {
+		return ResponseEntity.ok(ticketService.createTicket(keywordDTO.getId()));
 	}
 }
