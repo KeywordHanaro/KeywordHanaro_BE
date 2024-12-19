1f776e46433538e4d71aaece0487203e8ae988e6
Sung Yeob Jung <dev.sungyeob@gmail.com>
2024-12-19 11:09:40 +0900

    [feat]keyword delete & test code

--- src/main/java/com/hana4/keywordhanaro/service/KeywordService.java
+++ src/main/java/com/hana4/keywordhanaro/service/KeywordService.java
@@ -1,7 +1,12 @@
 package com.hana4.keywordhanaro.service;
 
+import org.springframework.http.ResponseEntity;
+
 import com.hana4.keywordhanaro.model.dto.KeywordDto;
+import com.hana4.keywordhanaro.model.mapper.KeywordMapper;
 
 public interface KeywordService {
 	public KeywordDto createKeyword(KeywordDto keywordDto);
+
+	ResponseEntity<KeywordMapper.DeleteResponse> removeKeyword(Long id);
 }
--- src/main/java/com/hana4/keywordhanaro/service/KeywordServiceImpl.java
+++ src/main/java/com/hana4/keywordhanaro/service/KeywordServiceImpl.java
@@ -1,5 +1,9 @@
 package com.hana4.keywordhanaro.service;
 
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.hana4.keywordhanaro.exception.InvalidRequestException;
@@ -70,5 +74,17 @@ public class KeywordServiceImpl implements KeywordService {
 
 		keyword = keywordRepository.save(keyword);
 		return KeywordMapper.toDto(keyword);
+	}
+
+	@Override
+	public ResponseEntity<KeywordMapper.DeleteResponse> removeKeyword(Long id) {
+		Optional<Keyword> keyword = keywordRepository.findById(id);
+		if (keyword.isPresent()) {
+			keywordRepository.delete(keyword.get());
+			return ResponseEntity.ok(new KeywordMapper.DeleteResponse(true, "Keyword deleted successfully"));
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+				.body(new KeywordMapper.DeleteResponse(false, "Keyword not found"));
+		}
 	}
 }
