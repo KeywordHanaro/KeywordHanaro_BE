a552ac010dea47834a525a43e1822193d24d484e
99andrew99 <whalstjr199927@gmail.com>
2024-12-20 12:55:00 +0900

    [feat] document swagger 개발

--- src/main/java/com/hana4/keywordhanaro/service/DocumentServiceImpl.java
+++ src/main/java/com/hana4/keywordhanaro/service/DocumentServiceImpl.java
@@ -2,6 +2,7 @@ package com.hana4.keywordhanaro.service;
 
 import org.springframework.stereotype.Service;
 
+import com.hana4.keywordhanaro.exception.InvalidRequestException;
 import com.hana4.keywordhanaro.model.dto.DocumentDto;
 import com.hana4.keywordhanaro.model.mapper.DocumentMapper;
 import com.hana4.keywordhanaro.repository.DocumentRepository;
@@ -16,6 +17,30 @@ public class DocumentServiceImpl implements DocumentService {
 
 	@Override
 	public void addDocument(DocumentDto documentDto) {
+		if (documentDto.getAmount() == null) {
+			throw new InvalidRequestException("There is no amount in request");
+		}
+		switch (documentDto.getType()) {
+			case DEPOSIT:
+				if (documentDto.getSubAccount() == null) {
+					throw new InvalidRequestException("There is no deposit target");
+				}
+
+				break;
+			case WITHDRAW:
+				if (documentDto.getAccount() == null) {
+					throw new InvalidRequestException("There is no withdraw target");
+				}
+
+				break;
+			case TRANSFER:
+				if (documentDto.getAccount() == null && documentDto.getSubAccount() == null) {
+					throw new InvalidRequestException("There is no transfer target");
+				}
+				break;
+			default:
+				throw new InvalidRequestException("Empty Document");
+		}
 		documentRepository.save(DocumentMapper.toEntity(documentDto));
 	}
 }
