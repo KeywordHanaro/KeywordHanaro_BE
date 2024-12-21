acd0cb12982513077f58f3bb9d73890564161179
ninw0205 <ninw0205@gmail.com>
2024-12-21 17:19:33 +0900

    [fix] : SwaggerUI jwt 넣어서 try it out 할수있게 수정

--- src/main/java/com/hana4/keywordhanaro/config/SpringConfig.java
+++ src/main/java/com/hana4/keywordhanaro/config/SpringConfig.java
@@ -6,14 +6,22 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SpringConfig {
 	@Bean
 	public OpenAPI openAPI() {
 		return new OpenAPI()
-			.components(new Components())
-			.info(info());
+			.components(new Components()
+				.addSecuritySchemes("bearerAuth",
+					new SecurityScheme()
+						.type(SecurityScheme.Type.HTTP)
+						.scheme("bearer")
+						.bearerFormat("JWT")))
+			.info(info())
+			.addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
 	}
 
 	private Info info() {
