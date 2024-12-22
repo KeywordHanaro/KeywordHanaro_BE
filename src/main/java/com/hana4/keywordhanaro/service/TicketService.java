94d8e5560f242b38fb60b242d2c5a257c7a9af67
insun-k <kis8294@naver.com>
2024-12-22 23:56:33 +0900

    UserMapper 수정

--- src/main/java/com/hana4/keywordhanaro/service/TicketService.java
+++ src/main/java/com/hana4/keywordhanaro/service/TicketService.java
@@ -1,11 +1,8 @@
 package com.hana4.keywordhanaro.service;
 
-import org.springframework.stereotype.Service;
-
 import com.hana4.keywordhanaro.model.dto.TicketDto;
 import com.hana4.keywordhanaro.model.dto.TicketRequestDto;
 
-@Service
 public interface TicketService {
 	TicketDto createTicket(TicketRequestDto ticketRequestDTO) throws Exception;
 }
