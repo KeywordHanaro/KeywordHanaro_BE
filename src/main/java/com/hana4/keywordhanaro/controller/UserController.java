f844ba7bf4ff29dd0df2e298267b416facc3177c
ninw0205 <ninw0205@gmail.com>
2024-12-27 16:37:14 +0900

    [refactor] : 마스터 비밀번호 검증 엔드포인트 변경

--- src/main/java/com/hana4/keywordhanaro/controller/UserController.java
+++ src/main/java/com/hana4/keywordhanaro/controller/UserController.java
@@ -35,7 +35,7 @@ public class UserController {
 		@ApiResponse(responseCode = "401", description = "인증 실패"),
 		@ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음")
 	})
-	@GetMapping("/checkMasterPassword")
+	@GetMapping("/auth-master")
 	public ResponseEntity<Boolean> checkMasterPassword(@RequestBody UserDto userDto, Authentication authentication) throws
 		UserNotFoundException {
 		return ResponseEntity.ok(userService.checkMasterPassword(authentication.getName(), userDto.getMasterPassword()));
